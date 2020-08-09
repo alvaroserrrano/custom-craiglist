e971e64200c39b75174685507c006a2a32e10758
Alvaro <alvaro.serrano@wmich.edu>
2020-08-09 20:20:26 +0200

    starting to scrape craiglist for search results

--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from bs4 import BeautifulSoup
+import requests
+from requests.compat import quote_plus
+
+BASE_CRAIGLIST_URL = 'https://detroit.craigslist.org/search/?query={}'
 
 def home(request):
     return render(request, 'base.html')
 
 def new_search(request):
-    return HttpResponse('Hello from new search')
+    search = request.POST.get('search')
+    final_url=f'BASE_CRAIGLIST_URL{quote_plus(search)}'
+    print(final_url)
+    context={
+        'search': search
+    }
+    return render(request, 'myapp/new_search.html', context)
